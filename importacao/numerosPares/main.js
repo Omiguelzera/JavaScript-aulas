4606fdf4e9abf62351ff787145087dbcf672c08c
Miguel Gs <miguelgsantos98@gmail.com>
2023-12-06 12:08:40 -0300

    exercício completo

--- importacao/numerosPares/main.js
+++ importacao/numerosPares/main.js
@@ -1,2 +1,13 @@
 const {gets} = require ('./numerosParesArray');
 
+const numeros = gets();
+
+for (i = 0; i < numeros; i ++){
+         const numero = gets();;
+
+         if(numero %2 === 0){
+
+            console.log(numero);
+         }
+
+}
