78a562e708a38cd55021cdc5251d8a62cba9538f
Miguel Gs <miguelgsantos98@gmail.com>
2024-01-08 21:16:14 -0300

    Create index.js

---
+++ Asin_Promise/index.js
@@ -0,0 +1,18 @@
+const promessaDeUmNumeroQualquer = new Promise ((resolve, reject) => {
+
+    const numero = parseInt(Math.radom() = 100)
+    resolve(numero)
+})
+
+
+
+promessaDeUmNumeroQualquer
+        .then((value) => {
+            console.log(value)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+        .finally(() => {
+            console.log("Finalizou ")
+        })
